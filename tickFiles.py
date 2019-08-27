c86e64771241d4918246344d7b9bd4b77b2ac204
Josh Kalina <jdkalina@gmail.com>
2019-08-27 09:51:22 -0500

    Update tickFiles.py

--- tickFiles.py
+++ tickFiles.py
@@ -26,9 +26,14 @@ def generateToken(myUsername = '',myPassword = ''):
         print ('Replace myUserName and myPassword with valid credentials, then repeat the request')
     return token
 
-
-def generateRequestBody(token, dataframe, index, ric_column = 'coIds', time_col = 'timestamp'):
-    """This function is meant to generate json request body and headers for a later post to tick history servers via api.
+def logCalls(ric, timestamp, start, end, file = '/home/josh/Documents/trthlogfile.txt'):
+    with open(file, 'a') as f:
+        mytime = time.strftime('%Y-%m-%d %H:%M', time.localtime())
+        string = mytime + ':\n\t' + ric + '\n\tStarting call range:\t' + str(start) + '\n\tStorytime:\t' + str(timestamp) + '\n\tending call range:\t' + end
+        f.write(string + '\n\n')
+        
+def generateRequestBody(token, dataframe, index, ric_column = 'subjects', time_col = 'timestamp'):
+    """
 
     :param token: the token received from Authentication call to TRTH servers in step 1
     :param dataframe: should be the output of the mrnReader methods
@@ -39,8 +44,8 @@ def generateRequestBody(token, dataframe, index, ric_column = 'coIds', time_col 
     """
     rics = dataframe.iloc[index][ric_column]
     timesDf = pd.DataFrame(data = {'max': [0], 'min':[0]})
-    timesDf['min'] = pd.to_datetime(dataframe[time_col]) - pd.Timedelta(minutes=2)
-    timesDf['max'] = pd.to_datetime(dataframe[time_col]) + pd.Timedelta(minutes=2)
+    timesDf['min'] = pd.to_datetime(dataframe.iloc[index][time_col]) - pd.Timedelta(minutes=3)
+    timesDf['max'] = pd.to_datetime(dataframe.iloc[index][time_col]) + pd.Timedelta(minutes=3)
     timesDf['min'] = timesDf['min'].dt.strftime('%Y-%m-%dT%H:%M:%S.000Z')
     timesDf['max'] = timesDf['max'].dt.strftime('%Y-%m-%dT%H:%M:%S.000Z')
     requestUrl='https://hosted.datascopeapi.reuters.com/RestApi/v1/Extractions/ExtractRaw'
@@ -55,12 +60,12 @@ def generateRequestBody(token, dataframe, index, ric_column = 'coIds', time_col 
         "ExtractionRequest": {
             "@odata.type": "#ThomsonReuters.Dss.Api.Extractions.ExtractionRequests.TickHistoryTimeAndSalesExtractionRequest",
             "ContentFieldNames": ['Quote - Ask Price',
-               'Quote - Bid Price',
-               'Quote - Bid Size',
-               'Trade - Market VWAP',
-               'Trade - Price',
-               'Trade - Volatility',
-               'Trade - Volume'],
+                                  'Quote - Bid Price',
+                                  'Quote - Bid Size',
+                                  'Trade - Market VWAP',
+                                  'Trade - Price',
+                                  'Trade - Volatility',
+                                  'Trade - Volume'],
             "IdentifierList": {
                 "@odata.type": "#ThomsonReuters.Dss.Api.Extractions.ExtractionRequests.InstrumentIdentifierList",
                 "InstrumentIdentifiers": []
@@ -74,81 +79,12 @@ def generateRequestBody(token, dataframe, index, ric_column = 'coIds', time_col 
             }
         }
     }
+    logrics = ''
     for ric in rics:
         instType, inst = ric.split(":")
         if instType == "R":
             requestBody["ExtractionRequest"]["IdentifierList"]["InstrumentIdentifiers"].append({"Identifier": inst,"IdentifierType": "Ric"})
+            logrics = inst + ',' + logrics
 
+    logCalls(logrics, dataframe.iloc[index][time_col],timesDf.iloc[0]['min'], timesDf.iloc[0]['max'])
     return requestUrl,requestHeaders,requestBody
-
-
-def generateFiles(token, requestUrl,requestHeaders,requestBody, iterator = 0 ,filePath = "/home/josh/Documents/mrnout/"):
-    r2 = requests.post(requestUrl, json=requestBody,headers=requestHeaders)
-    status_code = r2.status_code
-    print ("\tSTATUS: HTTP status of the response: " + str(status_code))
-    if status_code == 202 :
-        requestUrl = r2.headers["location"]
-        print ('\tSTATUS: Begin Polling Pickup Location URL')
-
-        requestHeaders={
-            "Prefer":"respond-async",
-            "Content-Type":"application/json",
-            "Authorization":"token " + token
-        }
-
-        #As long as the status of the request is 202, the extraction is not finished;
-        #we must wait, and poll the status until it is no longer 202:
-        while (status_code == 202):
-            print ('\tSTATUS: 202; waiting 15 seconds to poll again (need status 200 to continue)')
-            time.sleep(15)
-            r3 = requests.get(requestUrl,headers=requestHeaders)
-            status_code = r3.status_code
-
-        #When the status of the request is 200 the extraction is complete;
-        #we retrieve and display the jobId and the extraction notes (it is recommended to analyse their content)):
-        if status_code == 200 :
-            print('\tSTATUS: 200')
-            r3Json = json.loads(r3.text.encode('ascii', 'ignore'))
-            jobId = r3Json["JobId"]
-
-        #If instead of a status 200 we receive a different status, there was an error:
-        if status_code != 200 :
-            print ('ERROR: An error occurred. Try to run this cell again. If it fails, re-run the previous cell.\n')
-
-        requestUrl = "https://hosted.datascopeapi.reuters.com/RestApi/v1/Extractions/RawExtractionResults" + "('" + jobId + "')" + "/$value"
-        useAws = True
-
-        if useAws:
-            requestHeaders={
-                "Prefer":"respond-async",
-                "Content-Type":"text/plain",
-                "Accept-Encoding":"gzip",
-                "X-Direct-Download":"true",
-                "Authorization": "token " + token
-            }
-        else:
-            requestHeaders={
-                "Prefer":"respond-async",
-                "Content-Type":"text/plain",
-                "Accept-Encoding":"gzip",
-                "Authorization": "token " + token
-            }
-
-        r5 = requests.get(requestUrl,headers=requestHeaders,stream=True)
-        r5.raw.decode_content = False
-        if useAws:
-            print ('Content response headers (AWS server): type: ' + r5.headers["Content-Type"] + '\n')
-            #AWS does not set header Content-Encoding="gzip".
-        else:
-            print ('Content response headers (TRTH server): type: ' + r5.headers["Content-Type"] + ' - encoding: ' + r5.headers["Content-Encoding"] + '\n')
-
-        fileNameRoot = 'tickfile' + str(iterator)
-        fileName = filePath + fileNameRoot + ".step5.csv.gz"
-
-        print ('Saving compressed data to file:' + fileName)
-        chunk_size = 1024
-        rr = r5.raw
-        with open(fileName, 'wb') as fd:
-            shutil.copyfileobj(rr, fd, chunk_size)
-        return fileName
-        print ('\tSTATUS: Finished saving compressed data to file:' + fileName + '\n')
