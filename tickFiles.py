ae4fee655caa183679499349c27ef440db189fb6
Josh Kalina <jdkalina@gmail.com>
2019-10-15 20:20:28 -0500

    Update tickFiles.py

--- tickFiles.py
+++ tickFiles.py
@@ -88,3 +88,80 @@ def generateRequestBody(token, dataframe, index, ric_column = 'subjects', time_c
 
     logCalls(logrics, dataframe.iloc[index][time_col],timesDf.iloc[0]['min'], timesDf.iloc[0]['max'])
     return requestUrl,requestHeaders,requestBody
+
+
+def generateFiles(token, requestUrl,requestHeaders,requestBody, filePath = "/home/josh/Documents/tickFiles/"):
+    r2 = requests.post(requestUrl, json=requestBody,headers=requestHeaders)
+    status_code = r2.status_code
+    print ("\tSTATUS: HTTP status of the response: " + str(status_code))
+    if status_code == 202 :
+        requestUrl = r2.headers["location"]
+        print ('\tSTATUS: Begin Polling Pickup Location URL')
+
+        requestHeaders={
+            "Prefer":"respond-async",
+            "Content-Type":"application/json",
+            "Authorization":"token " + token
+        }
+
+        #As long as the status of the request is 202, the extraction is not finished;
+        #we must wait, and poll the status until it is no longer 202:
+        while (status_code == 202):
+            print ('\tSTATUS: 202; waiting 30 seconds to poll again (need status 200 to continue)')
+            time.sleep(30)
+            r3 = requests.get(requestUrl,headers=requestHeaders)
+            status_code = r3.status_code
+
+        #When the status of the request is 200 the extraction is complete;
+        #we retrieve and display the jobId and the extraction notes (it is recommended to analyse their content)):
+        if status_code == 200 :
+            print('\tSTATUS: 200')
+            r3Json = json.loads(r3.text.encode('ascii', 'ignore'))
+            jobId = r3Json["JobId"]
+
+        #If instead of a status 200 we receive a different status, there was an error:
+        if status_code != 200 :
+            print ('ERROR: An error occurred. Try to run this cell again. If it fails, re-run the previous cell.\n')
+
+        requestUrl = "https://hosted.datascopeapi.reuters.com/RestApi/v1/Extractions/RawExtractionResults" + "('" + jobId + "')" + "/$value"
+        useAws = True
+
+        if useAws:
+            requestHeaders={
+                "Prefer":"respond-async",
+                "Content-Type":"text/plain",
+                "Accept-Encoding":"gzip",
+                "X-Direct-Download":"true",
+                "Authorization": "token " + token
+            }
+        else:
+            requestHeaders={
+                "Prefer":"respond-async",
+                "Content-Type":"text/plain",
+                "Accept-Encoding":"gzip",
+                "Authorization": "token " + token
+            }
+
+        r5 = requests.get(requestUrl,headers=requestHeaders,stream=True)
+        r5.raw.decode_content = False
+        if useAws:
+            print ('Content response headers (AWS server): type: ' + r5.headers["Content-Type"] + '\n')
+            #AWS does not set header Content-Encoding="gzip".
+        else:
+            print ('Content response headers (TRTH server): type: ' + r5.headers["Content-Type"] + ' - encoding: ' + r5.headers["Content-Encoding"] + '\n')
+
+        fileNameRoot = file.replace(',csv','')
+        fileName = filePath + fileNameRoot + ".step5.csv.gz"
+
+        print ('Saving compressed data to file:' + fileName)
+        chunk_size = 1024
+        rr = r5.raw
+        with open(fileName, 'wb') as fd:
+            shutil.copyfileobj(rr, fd, chunk_size)
+
+        print ('\tSTATUS: Finished saving compressed data to file:' + fileName + '\n')
+
+        if ( (index+1) % 10) == 0:
+            print("PROGRESS: Finished file" + str(index) + ' of ' + str(len(sortFiles)))
+        index+=1
+
